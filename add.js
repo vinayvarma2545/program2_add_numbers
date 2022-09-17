86829359e671d86b5380d87b80c9580c288e9a2a
vinay <vinayvarma2545@gmail.com>
2022-09-17 12:29:22 +0530

    addNumbers

---
+++ add.js
@@ -0,0 +1,14 @@
+// simple
+
+// const x=5;
+// const y=10;
+// const z=x+y;
+// console.log(z)
+
+
+// input by user
+
+const x = parseInt(prompt('enter x value'));
+const y = parseInt(prompt('enter y value'));
+const z = x+y;
+document.write(`the sum of ${x} and ${y} is ${z}`)
